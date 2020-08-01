0230a8b14be6e13e127122a58edc5ca815d9f287
Khateeb Anwer <39136324+Khateebxtreme@users.noreply.github.com>
2020-08-01 23:55:39 +0530

    Update POTENTIOMETER_BASIC.ino

--- Fundamentals/9 - POTENTIOMETER_BASIC/POTENTIOMETER_BASIC.ino
+++ Fundamentals/9 - POTENTIOMETER_BASIC/POTENTIOMETER_BASIC.ino
@@ -2,12 +2,13 @@ void setup()
 {
   pinMode(A0, INPUT);
   pinMode(13,OUTPUT);
+  //default led is used
 }
 int SensorRead;
 void loop()
 {
   SensorRead=analogRead(A0);
-  if(SensorRead>500)
+  if(SensorRead>400)
     digitalWrite(13,HIGH);
   else{
     digitalWrite(13,LOW);
